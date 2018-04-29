d564a77c3db7448106978e627745b1ebd10af511
Goutham949n <Goutham949n@gmail.com>
2018-04-29 19:12:23 -0400

    TestMethodRef127

---
+++ src/com/test/testFromBehind/TestMethodRef127.java
@@ -0,0 +1,29 @@
+package com.test.testFromBehind;
+
+import java.util.Arrays;
+
+public class TestMethodRef127 {
+
+	public static void main(String[] args) {
+
+		String[] strArray = new String[] { "Tiger", "Rat", "Cat", "Lion" };
+		
+		Arrays.sort(strArray, CheckClass :: checkValue);
+
+
+		for (String s : strArray) { 
+			
+			System.out.print (s + " ");
+		}
+
+	}
+
+}
+
+class CheckClass {
+
+	public static int checkValue(String s1, String s2) {
+		return s1.length() - s2.length();
+	}
+
+}
